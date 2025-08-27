a1df90df0c99ba3167ea3ea65203680d0b18702d
tamagoman <dierocam@gmail.com>
2025-08-27 17:31:35 -0500

    Create menu itens and list

---
+++ lib/config/menu/menu_items.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+
+  MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon
+  });
+}
+
+final appMenuItems = <MenuItem>[
+  // MenuItem(
+  //   title: 'Counter Screen',
+  //   subTitle: 'Contador con StatefulWidget',
+  //   link: '/counter',
+  //   icon: Icons.add_circle_outline
+  // ),
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+  MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/cards',
+    icon: Icons.credit_card
+  ),
+  // MenuItem(
+  //   title: 'Progress Indicator',
+  //   subTitle: 'Generales y controlados',
+  //   link: '/progress',
+  //   icon: Icons.refresh_rounded
+  // ),
+  // MenuItem(
+  //   title: 'Snackbars y Dialogs',
+  //   subTitle: 'Indicadores en pantalla',
+  //   link: '/snackbars',
+  //   icon: Icons.info_outline
+  // ),
+  // MenuItem(
+  //   title: 'Animated Container',
+  //   subTitle: 'StatefulWidget animado',
+  //   link: '/animated-container',
+  //   icon: Icons.check_box_outline_blank_rounded
+  // ),
+  // MenuItem(
+  //   title: 'UI Controls + Tiles',
+  //   subTitle: 'Checkbox, Radio y Switch',
+  //   link: '/ui-controls',
+  //   icon: Icons.check_circle_outline
+  // ),
+  // MenuItem(
+  //   title: 'App Tutorial',
+  //   subTitle: 'Un pequeño tutorial de la app',
+  //   link: '/tutorial',
+  //   icon: Icons.school_outlined
+  // ),
+  // MenuItem(
+  //   title: 'Infinite Scroll & Pull to Refresh',
+  //   subTitle: 'Listas infinitas y pull to refresh',
+  //   link: '/infinite-scroll',
+  //   icon: Icons.list_alt_rounded
+  // ),
+  // MenuItem(
+  //   title: 'Change Theme',
+  //   subTitle: 'Cambiar tema de la app',
+  //   link: '/theme-changer',
+  //   icon: Icons.color_lens_outlined
+  // ),
+];
