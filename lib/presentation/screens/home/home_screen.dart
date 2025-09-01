659db973b25a8eba67ae64dcdb059de4f15eae17
tamagoman <dierocam@gmail.com>
2025-09-01 11:02:24 -0500

    Routes with Navigator()

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
+//import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -52,7 +53,13 @@ class _CustomListTile extends StatelessWidget {
       title: Text( menuItem.title ),
       subtitle: Text(menuItem.subTitle ),
       onTap: () => {
-        // TODO: navegar a la pantalla
+        // Navigator.of(context).push(
+        //   MaterialPageRoute<void>(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // )
+
+        Navigator.pushNamed(context, menuItem.link)
       },
     );
   }
